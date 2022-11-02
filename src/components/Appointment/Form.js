cf7ecd0d857b357424651e0c357d3301eac858c4
nicohsfu <marcnicohernandez@gmail.com>
2022-11-02 15:00:45 -0700

    displaying appointments - show and empty views

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import InterviewerList from "../InterviewerList";
+import Button from "../Button";
+
+export default function Form(props) {
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const cancel = () => {
+    props.onCancel();
+    reset();
+  };
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const save = () => {
+    props.onSave(student, interviewer);
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+              This must be a controlled component
+              your code goes here
+            */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          /* your code goes here */
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
+          value={interviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <Fragment>
+      <article className="appointment">
+        <Header time={props.time} />
+
+        <Fragment>
+          {props.interview ?
+            <>
+              <Show 
+                student={props.interview.student} 
+                interviewer={props.interview.interviewer} 
+                />
+            </>
+            :
+            <>
+              <Empty />
+            </>
+          }
+        </Fragment>
+
+      </article>
+    </Fragment>
   );
 }
