0f2d3933674794ccb980c776f0daee5d4e774d8b
nicohsfu <marcnicohernandez@gmail.com>
2022-11-07 17:21:11 -0800

    separation of concerns - moved logic out of the main application component

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData(initial) {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  function bookInterview(id, interview) {
+    // console.log("id and interview", id, interview);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    // PUT always takes in 2 args, second arg takes an object in this case
+    return axios.put(`/api/appointments/${id}`, { interview })
+      .then(() => {
+        setState({ ...state, appointments });
+      });
+  }
+
+  function cancelInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.delete(`/api/appointments/${id}`)
+      .then(() => {
+        setState({ ...state, appointments });
+      });
+  }
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      // console.log("all[0].data: ", all[0].data); // /api/days
+      // console.log("all[1].data: ", all[1].data); // /api/appointments
+      // console.log("all[2].data: ", all[2].data); // /api/interviewers
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    });
+  }, []);
+
+  return { state, setDay, bookInterview, cancelInterview };
+}
