9e79bf0b737c3fc58fb6aa5af7fb63d226e44d3c
nicohsfu <marcnicohernandez@gmail.com>
2022-11-08 12:37:33 -0800

    initialize prop-types on interviewList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./InterviewerList.scss";
 import InterviewerListItem from './InterviewerListItem';
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   const interviewers = props.interviewers;
@@ -25,3 +26,6 @@ export default function InterviewerList(props) {
   );
 }
 
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
